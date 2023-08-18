4e5d59217ff63de932ef7c1b1b03533b591a380c
José Molina Colmenero <jmolinacolmenero@gmail.com>
2023-08-18 20:04:20 +0200

    Upgrade polkadot v1.0.0 (#34)
    * feat: upgrade to polkadot-v0.9.43
    
    * feat: migrate to polkadot-v1.0.0

--- src/mock.rs
+++ src/mock.rs
@@ -1,26 +1,21 @@
 use crate::{self as pallet_chess};
 use frame_support::{
     parameter_types,
-    traits::{AsEnsureOriginWithArg, ConstU16, ConstU32, ConstU64, GenesisBuild},
+    traits::{AsEnsureOriginWithArg, ConstU16, ConstU32, ConstU64},
     PalletId,
 };
 use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
-    testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
+    BuildStorage,
 };
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-    pub enum Test where
-        Block = Block,
-        NodeBlock = Block,
-        UncheckedExtrinsic = UncheckedExtrinsic,
-    {
+    pub struct Test {
         System: frame_system,
         Balances: pallet_balances,
         Assets: pallet_assets,
@@ -29,21 +24,20 @@ frame_support::construct_runtime!(
 );
 
 impl system::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
     type BaseCallFilter = frame_support::traits::Everything;
     type BlockWeights = ();
     type BlockLength = ();
-    type DbWeight = ();
     type RuntimeOrigin = RuntimeOrigin;
     type RuntimeCall = RuntimeCall;
-    type Index = u64;
-    type BlockNumber = u64;
+    type Nonce = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
     type AccountId = u64;
     type Lookup = IdentityLookup<Self::AccountId>;
-    type Header = Header;
-    type RuntimeEvent = RuntimeEvent;
+    type Block = Block;
     type BlockHashCount = ConstU64<250>;
+    type DbWeight = ();
     type Version = ();
     type PalletInfo = PalletInfo;
     type AccountData = pallet_balances::AccountData<u64>;
@@ -78,24 +72,25 @@ impl pallet_chess::Config for Test {
 }
 
 impl pallet_balances::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
     type Balance = u64;
     type DustRemoval = ();
-    type RuntimeEvent = RuntimeEvent;
     type ExistentialDeposit = ConstU64<1>;
     type AccountStore = System;
-    type WeightInfo = ();
+    type ReserveIdentifier = [u8; 8];
+    type RuntimeHoldReason = ();
+    type FreezeIdentifier = ();
     type MaxLocks = ();
     type MaxReserves = ();
-    type ReserveIdentifier = [u8; 8];
-    type FreezeIdentifier = ();
-    type HoldIdentifier = ();
-    type MaxFreezes = ConstU32<0>;
     type MaxHolds = ConstU32<0>;
+    type MaxFreezes = ConstU32<0>;
 }
 
 impl pallet_assets::Config for Test {
     type RuntimeEvent = RuntimeEvent;
     type Balance = u64;
+    type RemoveItemsLimit = ConstU32<5>;
     type AssetId = u32;
     type AssetIdParameter = u32;
     type Currency = Balances;
@@ -108,10 +103,9 @@ impl pallet_assets::Config for Test {
     type ApprovalDeposit = ConstU64<1>;
     type StringLimit = ConstU32<50>;
     type Freezer = ();
-    type WeightInfo = ();
     type Extra = ();
-    type RemoveItemsLimit = ConstU32<5>;
     type CallbackHandle = ();
+    type WeightInfo = ();
 }
 
 pub const ASSET_ID: u32 = 200u32;
@@ -125,8 +119,8 @@ frame_support::parameter_types! {
 // Build genesis storage according to the mock runtime.
 #[allow(dead_code)]
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let mut storage = system::GenesisConfig::default()
-        .build_storage::<Test>()
+    let mut storage = system::GenesisConfig::<Test>::default()
+        .build_storage()
         .unwrap();
     let asset_id = AssetId::get();
     let asset_min_balance = AssetMinBalance::get();
