5b378e106e5389ca08538fbee89296c297fdfe76
Steve Degosserie <steve@parity.io>
2023-10-30 20:57:39 +0100

    Start refactoring to move game execution off-chain.

--- src/lib.rs
+++ src/lib.rs
@@ -62,15 +62,9 @@ pub mod pallet {
     }
 
     #[derive(Clone, Debug, Encode, Decode, TypeInfo, PartialEq)]
-    pub enum NextMove {
-        Whites,
-        Blacks,
-    }
-
-    #[derive(Clone, Debug, Encode, Decode, TypeInfo, PartialEq)]
     pub enum MatchState {
         AwaitingOpponent,
-        OnGoing(NextMove),
+        OnGoing,
         Won,
         Drawn,
     }
@@ -80,13 +74,9 @@ pub mod pallet {
     pub struct Match<T: Config> {
         pub challenger: T::AccountId,
         pub opponent: T::AccountId,
-        // no need for BoundedVec, we only modify this internally (and safely)
-        // introducing an extra runtime constant would be overkill
-        pub board: Vec<u8>,
         pub state: MatchState,
         pub nonce: u128,
         pub style: MatchStyle,
-        pub last_move: BlockNumberFor<T>,
         pub start: BlockNumberFor<T>,
         pub bet_asset_id: AssetIdOf<T>,
         pub bet_amount: T::AssetBalance,
@@ -277,7 +267,6 @@ pub mod pallet {
         MatchCreated(T::AccountId, T::AccountId, T::Hash),
         MatchAborted(T::Hash),
         MatchStarted(T::Hash),
-        MoveExecuted(T::Hash, T::AccountId, Vec<u8>),
         MatchWon(T::Hash, T::AccountId, Vec<u8>),
         MatchDrawn(T::Hash, Vec<u8>),
         MatchRefundError(T::Hash),
@@ -293,20 +282,15 @@ pub mod pallet {
         NotMatchOpponent,
         NotMatchChallenger,
         InvalidBoardEncoding,
-        InvalidMoveEncoding,
         NotAwaitingOpponent,
         StillAwaitingOpponent,
         MatchAlreadyFinished,
-        NotYourTurn,
-        IllegalMove,
         BetTooLow,
         BetDoesNotExist,
         MatchNotOnGoing,
         MatchNotAbandoned,
         MoveNotExpired,
     }
-
-    const MOVE_FEN_LENGTH: usize = 4;
 
     type GenesisInfo<T> = (AccountIdOf<T>, u16);
 
@@ -353,11 +337,9 @@ pub mod pallet {
             let new_match: Match<T> = Match {
                 challenger: challenger.clone(),
                 opponent: opponent.clone(),
-                board: Self::init_board(),
                 state: MatchState::AwaitingOpponent,
                 nonce: nonce.clone(),
                 style,
-                last_move: 0u32.into(),
                 start: 0u32.into(),
                 bet_asset_id,
                 bet_amount,
@@ -424,7 +406,7 @@ pub mod pallet {
 
             chess_match.opponent_bet()?;
 
-            chess_match.state = MatchState::OnGoing(NextMove::Whites);
+            chess_match.state = MatchState::OnGoing;
             chess_match.start = <frame_system::Pallet<T>>::block_number();
             <Matches<T>>::insert(match_id, chess_match);
 
@@ -433,202 +415,89 @@ pub mod pallet {
             Ok(())
         }
 
-        #[pallet::call_index(3)]
-        #[pallet::weight(T::WeightInfo::make_move())]
-        pub fn make_move(
-            origin: OriginFor<T>,
-            match_id: T::Hash,
-            move_fen: Vec<u8>,
-        ) -> DispatchResult {
-            let who = ensure_signed(origin)?;
+        // TODO: to be refactored. Implements a dispute resolution mechanism whereby players
+        // can submit their latest play along with the zkVM execution proof. The play that will
+        // be deemed the latest (as per the fullmove counter) and verified, will determine who
+        // is the winner.
+        // #[pallet::call_index(4)]
+        // #[pallet::weight(T::WeightInfo::clear_abandoned_match())]
+        // pub fn clear_abandoned_match(origin: OriginFor<T>, match_id: T::Hash) -> DispatchResult {
+        //     let who = ensure_signed(origin)?;
+        //     let chess_match = match Self::chess_matches(match_id) {
+        //         Some(m) => m,
+        //         None => return Err(Error::<T>::NonExistentMatch.into()),
+        //     };
 
-            ensure!(
-                move_fen.len() == MOVE_FEN_LENGTH,
-                Error::<T>::InvalidMoveEncoding
-            );
+        //     ensure!(
+        //         (chess_match.state == MatchState::OnGoing(NextMove::Whites))
+        //             | (chess_match.state == MatchState::OnGoing(NextMove::Blacks)),
+        //         Error::<T>::MatchNotOnGoing
+        //     );
 
-            let mut chess_match: Match<T> = match Self::chess_matches(match_id) {
-                Some(m) => m,
-                None => return Err(Error::<T>::NonExistentMatch.into()),
-            };
+        //     let now = <frame_system::Pallet<T>>::block_number();
+        //     let diff = now - chess_match.last_move;
 
-            match chess_match.state {
-                MatchState::AwaitingOpponent => {
-                    return Err(Error::<T>::StillAwaitingOpponent.into())
-                }
-                MatchState::Won | MatchState::Drawn => {
-                    return Err(Error::<T>::MatchAlreadyFinished.into())
-                }
-                MatchState::OnGoing(NextMove::Whites) => {
-                    if who != chess_match.challenger {
-                        return Err(Error::<T>::NotYourTurn.into());
-                    }
-                }
-                MatchState::OnGoing(NextMove::Blacks) => {
-                    if who != chess_match.opponent {
-                        return Err(Error::<T>::NotYourTurn.into());
-                    }
-                }
-            }
+        //     let expired: bool = match chess_match.style {
+        //         MatchStyle::Bullet => diff > T::BulletPeriod::get(),
+        //         MatchStyle::Blitz => diff > T::BlitzPeriod::get(),
+        //         MatchStyle::Rapid => diff > T::RapidPeriod::get(),
+        //         MatchStyle::Daily => diff > T::DailyPeriod::get(),
+        //     };
 
-            let mut board_obj: Board = Self::decode_board(chess_match.board)?;
-            let move_obj: Move = Self::decode_move(move_fen.clone())?;
+        //     ensure!(expired, Error::<T>::MoveNotExpired);
 
-            if !board_obj.is_legal(move_obj) {
-                return Err(Error::<T>::IllegalMove.into());
-            }
+        //     let winner = match chess_match.state {
+        //         MatchState::OnGoing(NextMove::Whites) => chess_match.opponent.clone(),
+        //         _ => chess_match.challenger.clone(),
+        //     };
 
-            // we already checked for legality, so we call play_unchecked (faster)
-            board_obj.play_unchecked(move_obj);
+        //     Self::deposit_event(Event::MatchWon(
+        //         match_id,
+        //         winner.clone(),
+        //         chess_match.board.clone(),
+        //     ));
 
-            // check game status: Won? Drawn? OnGoing?
-            chess_match.state = match board_obj.status() {
-                GameStatus::Ongoing => match board_obj.side_to_move() {
-                    Color::White => MatchState::OnGoing(NextMove::Whites),
-                    Color::Black => MatchState::OnGoing(NextMove::Blacks),
-                },
-                GameStatus::Won => MatchState::Won,
-                GameStatus::Drawn => MatchState::Drawn,
-            };
+        //     let abandoned: bool = match chess_match.style {
+        //         MatchStyle::Bullet => diff > T::BulletPeriod::get() * 10u32.into(),
+        //         MatchStyle::Blitz => diff > T::BlitzPeriod::get() * 10u32.into(),
+        //         MatchStyle::Rapid => diff > T::RapidPeriod::get() * 10u32.into(),
+        //         MatchStyle::Daily => diff > T::DailyPeriod::get() * 10u32.into(),
+        //     };
 
-            chess_match.board = Self::encode_board(board_obj);
-            chess_match.last_move = <frame_system::Pallet<T>>::block_number();
+        //     if (who == chess_match.challenger) | (who == chess_match.opponent) | !abandoned {
+        //         // winner gets both deposits before match becomes abandoned
+        //         match chess_match.win_bet(&winner) {
+        //             Ok(()) => {}
+        //             Err(_) => Self::deposit_event(Event::MatchAwardError(match_id, winner.clone())),
+        //         }
+        //     } else {
+        //         // who cleared the match after match is abandoned gets the incentive,
+        //         // and the winner gets both deposits minus the incentive share
+        //         match chess_match.clear_abandoned_bet(&winner, &who) {
+        //             Ok(()) => {}
+        //             Err(_) => Self::deposit_event(Event::MatchClearanceError(
+        //                 match_id,
+        //                 winner.clone(),
+        //                 who,
+        //             )),
+        //         }
+        //     }
 
-            Self::deposit_event(Event::MoveExecuted(match_id, who.clone(), move_fen));
-            if chess_match.state == MatchState::Won {
-                Self::deposit_event(Event::MatchWon(
-                    match_id,
-                    who.clone(),
-                    chess_match.board.clone(),
-                ));
+        //     // update elo rating
+        //     let looser = if chess_match.challenger == winner {
+        //         chess_match.opponent.clone()
+        //     } else {
+        //         chess_match.challenger.clone()
+        //     };
+        //     Self::update_elo(winner.clone(), 1_f32, looser, 0_f32);
 
-                // winner gets both deposits
-                chess_match.win_bet(&who)?;
+        //     <Matches<T>>::remove(match_id);
+        //     <PlayerMatches<T>>::remove(chess_match.challenger.clone(), match_id);
+        //     <PlayerMatches<T>>::remove(chess_match.opponent, match_id);
+        //     <MatchIdFromNonce<T>>::remove(chess_match.nonce);
 
-                // update elo rating
-                let (score_1, score_2) = if chess_match.challenger == who {
-                    (1_f32, 0_f32)
-                } else {
-                    (0_f32, 1_f32)
-                };
-                Self::update_elo(
-                    chess_match.challenger.clone(),
-                    score_1,
-                    chess_match.opponent.clone(),
-                    score_2,
-                );
-
-                // match is over, clean up storage
-                <Matches<T>>::remove(match_id);
-                <PlayerMatches<T>>::remove(chess_match.challenger.clone(), match_id);
-                <PlayerMatches<T>>::remove(chess_match.opponent, match_id);
-                <MatchIdFromNonce<T>>::remove(chess_match.nonce);
-            } else if chess_match.state == MatchState::Drawn {
-                Self::deposit_event(Event::MatchDrawn(match_id, chess_match.board.clone()));
-
-                // return deposit to both players
-                chess_match.refund_bets()?;
-
-                // update elo rating
-                Self::update_elo(
-                    chess_match.challenger.clone(),
-                    0.5,
-                    chess_match.opponent.clone(),
-                    0.5,
-                );
-
-                // match is over, clean up storage
-                <Matches<T>>::remove(match_id);
-                <PlayerMatches<T>>::remove(chess_match.challenger.clone(), match_id);
-                <PlayerMatches<T>>::remove(chess_match.opponent, match_id);
-                <MatchIdFromNonce<T>>::remove(chess_match.nonce);
-            } else {
-                // match still ongoing, update on-chain board
-                <Matches<T>>::insert(match_id, chess_match);
-            }
-
-            Ok(())
-        }
-
-        #[pallet::call_index(4)]
-        #[pallet::weight(T::WeightInfo::clear_abandoned_match())]
-        pub fn clear_abandoned_match(origin: OriginFor<T>, match_id: T::Hash) -> DispatchResult {
-            let who = ensure_signed(origin)?;
-            let chess_match = match Self::chess_matches(match_id) {
-                Some(m) => m,
-                None => return Err(Error::<T>::NonExistentMatch.into()),
-            };
-
-            ensure!(
-                (chess_match.state == MatchState::OnGoing(NextMove::Whites))
-                    | (chess_match.state == MatchState::OnGoing(NextMove::Blacks)),
-                Error::<T>::MatchNotOnGoing
-            );
-
-            let now = <frame_system::Pallet<T>>::block_number();
-            let diff = now - chess_match.last_move;
-
-            let expired: bool = match chess_match.style {
-                MatchStyle::Bullet => diff > T::BulletPeriod::get(),
-                MatchStyle::Blitz => diff > T::BlitzPeriod::get(),
-                MatchStyle::Rapid => diff > T::RapidPeriod::get(),
-                MatchStyle::Daily => diff > T::DailyPeriod::get(),
-            };
-
-            ensure!(expired, Error::<T>::MoveNotExpired);
-
-            let winner = match chess_match.state {
-                MatchState::OnGoing(NextMove::Whites) => chess_match.opponent.clone(),
-                _ => chess_match.challenger.clone(),
-            };
-
-            Self::deposit_event(Event::MatchWon(
-                match_id,
-                winner.clone(),
-                chess_match.board.clone(),
-            ));
-
-            let abandoned: bool = match chess_match.style {
-                MatchStyle::Bullet => diff > T::BulletPeriod::get() * 10u32.into(),
-                MatchStyle::Blitz => diff > T::BlitzPeriod::get() * 10u32.into(),
-                MatchStyle::Rapid => diff > T::RapidPeriod::get() * 10u32.into(),
-                MatchStyle::Daily => diff > T::DailyPeriod::get() * 10u32.into(),
-            };
-
-            if (who == chess_match.challenger) | (who == chess_match.opponent) | !abandoned {
-                // winner gets both deposits before match becomes abandoned
-                match chess_match.win_bet(&winner) {
-                    Ok(()) => {}
-                    Err(_) => Self::deposit_event(Event::MatchAwardError(match_id, winner.clone())),
-                }
-            } else {
-                // who cleared the match after match is abandoned gets the incentive,
-                // and the winner gets both deposits minus the incentive share
-                match chess_match.clear_abandoned_bet(&winner, &who) {
-                    Ok(()) => {}
-                    Err(_) => Self::deposit_event(Event::MatchClearanceError(
-                        match_id,
-                        winner.clone(),
-                        who,
-                    )),
-                }
-            }
-
-            // update elo rating
-            let looser = if chess_match.challenger == winner {
-                chess_match.opponent.clone()
-            } else {
-                chess_match.challenger.clone()
-            };
-            Self::update_elo(winner.clone(), 1_f32, looser, 0_f32);
-
-            <Matches<T>>::remove(match_id);
-            <PlayerMatches<T>>::remove(chess_match.challenger.clone(), match_id);
-            <PlayerMatches<T>>::remove(chess_match.opponent, match_id);
-            <MatchIdFromNonce<T>>::remove(chess_match.nonce);
-
-            Ok(())
-        }
+        //     Ok(())
+        // }
     }
 
     impl<T: Config> Pallet<T> {
@@ -645,14 +514,7 @@ pub mod pallet {
             T::Hashing::hash_of(&(challenger, opponent, nonce))
         }
 
-        fn init_board() -> Vec<u8> {
-            format!("{}", Board::default()).as_bytes().to_vec()
-        }
-
-        fn encode_board(board: Board) -> Vec<u8> {
-            format!("{}", board).as_bytes().to_vec()
-        }
-
+        
         fn decode_board(encoded_board: Vec<u8>) -> sp_std::result::Result<Board, Error<T>> {
             let s = match from_utf8(encoded_board.as_slice()) {
                 Ok(s) => s,
@@ -662,60 +524,6 @@ pub mod pallet {
                 Ok(g) => Ok(g),
                 Err(_) => Err(Error::<T>::InvalidBoardEncoding.into()),
             }
-        }
-
-        fn decode_move(encoded_move: Vec<u8>) -> sp_std::result::Result<Move, Error<T>> {
-            let s = match from_utf8(encoded_move.as_slice()) {
-                Ok(s) => s,
-                Err(_) => "",
-            };
-            match Move::from_str(s) {
-                Ok(m) => Ok(m),
-                Err(_) => Err(Error::<T>::InvalidMoveEncoding.into()),
-            }
-        }
-
-        // needed for benchmarking
-        // todo: check if pub is a vulnerability
-        pub fn force_board_state(
-            match_id: T::Hash,
-            encoded_board: Vec<u8>,
-        ) -> sp_std::result::Result<(), Error<T>> {
-            let mut chess_match = match Self::chess_matches(match_id) {
-                Some(m) => m,
-                None => return Err(Error::<T>::NonExistentMatch.into()),
-            };
-
-            chess_match.board = encoded_board.clone();
-
-            let board_obj = Self::decode_board(encoded_board)?;
-            chess_match.state = match board_obj.status() {
-                GameStatus::Ongoing => match board_obj.side_to_move() {
-                    Color::White => MatchState::OnGoing(NextMove::Whites),
-                    Color::Black => MatchState::OnGoing(NextMove::Blacks),
-                },
-                GameStatus::Won => MatchState::Won,
-                GameStatus::Drawn => MatchState::Drawn,
-            };
-
-            if chess_match.state == MatchState::Won {
-                // match is over, clean up storage
-                <Matches<T>>::remove(match_id);
-                <PlayerMatches<T>>::remove(chess_match.challenger.clone(), match_id);
-                <PlayerMatches<T>>::remove(chess_match.opponent, match_id);
-                <MatchIdFromNonce<T>>::remove(chess_match.nonce);
-            } else if chess_match.state == MatchState::Drawn {
-                // match is over, clean up storage
-                <Matches<T>>::remove(match_id);
-                <PlayerMatches<T>>::remove(chess_match.challenger.clone(), match_id);
-                <PlayerMatches<T>>::remove(chess_match.opponent, match_id);
-                <MatchIdFromNonce<T>>::remove(chess_match.nonce);
-            } else {
-                // match still ongoing, update on-chain board
-                <Matches<T>>::insert(match_id, chess_match);
-            }
-
-            Ok(())
         }
 
         fn update_elo(player1: T::AccountId, score_1: f32, player2: T::AccountId, score_2: f32) {
